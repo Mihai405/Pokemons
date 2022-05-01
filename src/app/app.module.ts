1a41183952c60a95b49bda9a0088c99c958a1fb8
Mihai405 <mihaijurj2000@yahoo.com>
2022-05-01 16:31:10 +0300

    Styling header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './header/header.component';import { SearchComponent } from './header/search/search.component';
+;
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    routingComponents,
-    PageNotFoundComponent
+    SearchComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
